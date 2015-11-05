b100dac12fa270107768fc725cf7aba7a26942cd
phealeyh <tranceformas_101@hotmail.com>
2015-11-06 00:15:11 +1100

    added preference xml

---
+++ app/src/main/java/tracking/id11723222/com/trackingapplication/SettingsActivity.java
@@ -0,0 +1,13 @@
+package tracking.id11723222.com.trackingapplication;
+
+import android.preference.PreferenceActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.timer_settings);
+    }
+}
