97e9f260dbcb2f667f897dc8ddf2b8fea137e797
phealeyh <tranceformas_101@hotmail.com>
2015-11-05 21:00:11 +1100

    chronometer added for interval timer

--- app/src/main/java/tracking/id11723222/com/trackingapplication/TrackingActivity.java
+++ app/src/main/java/tracking/id11723222/com/trackingapplication/TrackingActivity.java
@@ -4,11 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.Toast;
 
 public class TrackingActivity extends AppCompatActivity {
 
     private Button mStartButton, mResetButton, mEmailButton;
+    private Chronometer mIntervalChronomter;
 
 
     @Override
@@ -16,8 +18,18 @@ public class TrackingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracking);
         setButtonListeners();
+        setChronometer();
     }
 
+
+    /**
+     * This method will set each button listener to handle it's respective action events.
+     * If the start button is clicked, the TrackingService Intent is started along which will
+     * track the user's coordinate at each interval.
+     * The Reset values button will reset all of the values inside the lists
+     * The Email button will open up a new email intent and allow the user to email the locations
+     * and times recorded.
+     */
 
     private void setButtonListeners(){
         mStartButton = (Button)findViewById(R.id.startButton);
@@ -41,6 +53,15 @@ public class TrackingActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),Constants.EMAIL_SENT,Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    /**
+     * Sets the chronometer to time the intervals given.
+     */
+    private void setChronometer(){
+        mIntervalChronomter = (Chronometer) findViewById(R.id.interval_timer);
+        //set it to the interval given
+        mIntervalChronomter.setBase(5000L);
     }
 
 
